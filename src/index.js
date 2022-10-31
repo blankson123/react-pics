8eaa194daa6ebd284326c6ced18507b33e441fa5
Richmond Blankson <mensahrichmondb@gmail.com>
2022-10-31 21:07:20 +0000

    made app component to handle search term

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
-import ReactDOM  from "react-dom";
+import ReactDOM  from "react-dom/client";
 import App from './components/App';
 
+const el = document.getElementById('root');
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const root = ReactDOM.createRoot(el);
+
+
+root.render(<App/>);
 
